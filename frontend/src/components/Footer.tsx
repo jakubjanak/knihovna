7ca54ec984a8fd5267f9412589cc8137b07489c9
Jakub Janák <jakub.janak@icloud.com>
2025-04-22 12:10:39 +0200

    Footer komponenta hotová.

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="@container bg-gray-200 font-semibold flex justify-center items-center p-2">
+        <p className="text-xs">© Jakub Janák - 2025</p>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,10 +1,12 @@
 import Navigation from "../components/Navigation"
+import Footer from "../components/Footer"
 
 function Homepage() {
   return (
-    <>
+    <div className="flex flex-col gap-4 justify-between h-screen">
         <Navigation />
-    </>
+        <Footer />
+    </div>
   )
 }
 
