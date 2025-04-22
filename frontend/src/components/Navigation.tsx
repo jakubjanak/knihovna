2cc17785993878c1f4597bfc1c2313090472e184
Jakub Janák <jakub.janak@icloud.com>
2025-04-22 12:04:01 +0200

    Základní navigace hotová. Hamburgerové menu hotové. Příprava useStatů na stav přihlášení uživatele hotová.

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,22 +1,61 @@
+import { useState } from "react"
+
 function Navigation() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <nav className="@container flex justify-between">
-        <ul className="flex justify-evenly gap-10">
+    <nav className="@container flex flex-col gap-4 items-center md:flex-row md:justify-between bg-gray-200 p-4 font-semibold">
+        <ul className="flex justify-evenly gap-10 hidden md:flex">
             <li><a href="#">Google Books</a></li>
             <li><a href="#">Apple Books</a></li>
             <li><a href="#">Amazon Books</a></li>
         </ul>
-        <ul className="flex justify-evenly gap-10">
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Register</a></li>
+        <ul className="flex justify-evenly gap-10 hidden md:flex">
+            {
+              isLoggedIn ? (
+                <>
+                  <li><a href="#">My Books</a></li>
+                  <li><a href="#">My Reviews</a></li>
+                  <li><a href="#">My Profile</a></li>
+                </>
+              ) : (
+                <>
+                  <li><a href="#">Login</a></li>
+                  <li><a href="#">Register</a></li>
+                </>
+              )
+            }
         </ul>
+        <div className="md:hidden w-full flex justify-end">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 cursor-pointer md:hidden" onClick={toggleMenu}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </div>
+        <ul className={isOpen ? "md:hidden flex flex-col gap-4" : "hidden"}>
+            <li><a href="#">Google Books</a></li>
+            <li><a href="#">Apple Books</a></li>
+            <li><a href="#">Amazon Books</a></li>
 
-        {/* Pokud bude uživatel přihlášený */}
-        {/* <ul>
-            <li><a href="#">My Books</a></li>
-            <li><a href="#">My Reviews</a></li>
-            <li><a href="#">My Profile</a></li>
-        </ul> */}
+            {
+              isLoggedIn ? (
+                <>
+                  <li><a href="#">My Books</a></li>
+                  <li><a href="#">My Reviews</a></li>
+                  <li><a href="#">My Profile</a></li>
+                </>
+              ) : (
+                <>
+                  <li><a href="#">Login</a></li>
+                  <li><a href="#">Register</a></li>
+                </>
+              )
+            }
+        </ul>
     </nav>
   )
 }
